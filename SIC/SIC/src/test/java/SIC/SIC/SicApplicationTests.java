dc9814a44419d3a9faff7d0acb03500ba6470307
Angel9098 <90285177+Angel9098@users.noreply.github.com>
2023-07-25 11:05:32 -0600

    Add files via upload

---
+++ SIC/SIC/src/test/java/SIC/SIC/SicApplicationTests.java
@@ -0,0 +1,13 @@
+package SIC.SIC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
